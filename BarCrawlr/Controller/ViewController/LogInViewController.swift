86639e0ff5169d48575d4ea7d0cb8d1f9863a606
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-07-25 19:04:26 -0400

    set up initial log in view and started the view controller

---
+++ BarCrawlr/Controller/ViewController/LogInViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LogInViewController.swift
+//  BarCrawlr
+//
+//  Created by Jason Mandozzi on 7/25/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    
+    
+    //Outlets
+    
+    //Buttons
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var confirmButton: UIButton!
+    
+    //Text Fields
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //Actions
+    @IBAction func LogInButtonTapped(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func confirmButtonTapped(_ sender: Any) {
+        
+        
+    }
+    
+
+
+}
