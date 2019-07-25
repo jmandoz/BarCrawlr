5afb86a546e85d3a9c4f2862dd3cdb7e4fb601e1
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-07-25 12:16:05 -0400

    Set up the User Model

--- BarCrawlr/Model/User.swift
+++ BarCrawlr/Model/User.swift
@@ -26,5 +26,35 @@ class User {
         self.ckRecordID = ckRecordID
         self.appleUserReference = appleUserReference
     }
-    
+}
+
+//Extension - initializing User from CKRecord
+extension User {
+    convenience init?(record: CKRecord) {
+        guard let username = record[UserConstants.usernameKey] as? String,
+        let email = record[UserConstants.emailKey] as? String,
+            let password = record[UserConstants.passwordKey] as? String,
+            let appleUserReference = record[UserConstants.appleUserReferenceKey] as? CKRecord.Reference else {return nil}
+        
+        self.init(username: username, password: password, email: email, ckRecordID: record.recordID, appleUserReference: appleUserReference)
+    }
+}
+//Initializing a CKRecord from an existing User 
+extension CKRecord {
+    convenience init(user: User) {
+        self.init(recordType: UserConstants.typeKey, recordID: user.ckRecordID)
+        self.setValue(user.username, forKey: UserConstants.usernameKey)
+        self.setValue(user.password, forKey: UserConstants.passwordKey)
+        self.setValue(user.email, forKey: UserConstants.emailKey)
+        self.setValue(user.appleUserReference, forKey: UserConstants.appleUserReferenceKey)
+    }
+}
+
+//Magic Strings
+struct UserConstants {
+    static let typeKey = "User"
+    fileprivate static let usernameKey = "username"
+    fileprivate static let passwordKey = "password"
+    fileprivate static let emailKey = "email"
+    fileprivate static let appleUserReferenceKey = "appleUserReference"
 }
