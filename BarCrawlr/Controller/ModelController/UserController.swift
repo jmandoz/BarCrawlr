efcec5fa42e3555de9228c71519de7e174778dd2
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-07-26 18:30:12 -0600

    Implemented CloudKit check for Apple User

---
+++ BarCrawlr/Controller/ModelController/UserController.swift
@@ -0,0 +1,9 @@
+//
+//  UserController.swift
+//  BarCrawlr
+//
+//  Created by Jason Mandozzi on 7/26/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import Foundation
---
+++ BarCrawlr/Controller/ViewController/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  BarCrawlr
+//
+//  Created by Jason Mandozzi on 7/25/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
